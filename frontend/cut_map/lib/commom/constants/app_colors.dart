cef214ce77bb549edf5c9c3c241535eb1e57f031
Henrique Nunes <henriquesilvanunes0015@gmail.com>
2026-03-30 20:34:13 -0300

    style(front): adicionadas novas cores e fontes

--- frontend/cut_map/lib/commom/constants/app_colors.dart
+++ frontend/cut_map/lib/commom/constants/app_colors.dart
@@ -6,5 +6,7 @@ class AppColors {
   static const Color yellow = Color(0xFFF5C518);
   static const Color darkGray = Color(0xFF2A2A2A);
   static const Color gray = Color(0xFF888888);
-  static const Color background = Color(0xFF0D0D0D);
+  static const Color background = Color(0xFF121212);
+  static const Color red = Color(0xFFBA0D0D);
+  static const Color white = Color(0xFFEFEAEA);
 }
--- frontend/cut_map/lib/commom/constants/app_fonts.dart
+++ frontend/cut_map/lib/commom/constants/app_fonts.dart
@@ -8,4 +8,39 @@ class AppFonts {
     fontSize: 18,
     fontWeight: FontWeight.w400,
   );
+  static const TextStyle regular16 = TextStyle(
+    fontFamily: 'Roboto',
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle regular14 = TextStyle(
+    fontFamily: 'Roboto',
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle regular12 = TextStyle(
+    fontFamily: 'Roboto',
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle bold26 = TextStyle(
+    fontFamily: 'Roboto',
+    fontSize: 26,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle bold16 = TextStyle(
+    fontFamily: 'Roboto',
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle bold14 = TextStyle(
+    fontFamily: 'Roboto',
+    fontSize: 14,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle bold12 = TextStyle(
+    fontFamily: 'Roboto',
+    fontSize: 12,
+    fontWeight: FontWeight.bold,
+  );
 }
