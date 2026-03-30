777bce63c202f81624fa3c1403e1bdc7bc4be792
Henrique Nunes <henriquesilvanunes0015@gmail.com>
2026-03-30 11:57:33 -0300

    style(front): adiconadas as fontes da aplicação

---
+++ frontend/cut_map/lib/commom/constants/app_fonts.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+
+class AppFonts {
+  AppFonts._();
+
+  static const TextStyle regular18 = TextStyle(
+    fontFamily: 'Roboto',
+    fontSize: 18,
+    fontWeight: FontWeight.w400,
+  );
+}
