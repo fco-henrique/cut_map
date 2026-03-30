6e3275dfa040a64b771b75263e9e64fce16ef8f4
Henrique Nunes <henriquesilvanunes0015@gmail.com>
2026-03-30 20:32:27 -0300

    feat(front): adicionados os widgets custom password form field e o custom primary button

---
+++ frontend/cut_map/lib/commom/widgets/custom_password_form_field.dart
@@ -0,0 +1,77 @@
+// import 'package:cut_map/commom/constants/app_colors.dart';
+import 'package:cut_map/commom/constants/app_colors.dart';
+import 'package:cut_map/commom/extensions/sizes.dart';
+import 'package:cut_map/commom/widgets/custom_text_form_field.dart';
+import 'package:flutter/material.dart';
+
+class CustomPasswordFormField extends StatefulWidget {
+  final TextEditingController? textEditingController;
+  final EdgeInsetsGeometry? padding;
+  final String? hintText;
+  final String? labelText;
+  final FormFieldValidator<String>? validator;
+  final String? helperText;
+  final String? counterText;
+  final IconData? prefixIcon;
+  final int? maxLength;
+
+  const CustomPasswordFormField({
+    super.key,
+    this.textEditingController,
+    this.padding,
+    this.hintText,
+    this.labelText,
+    this.validator,
+    this.helperText,
+    this.prefixIcon,
+    this.maxLength,
+    this.counterText,
+  });
+
+  @override
+  State<CustomPasswordFormField> createState() =>
+      _CustomPasswordFormFieldState();
+}
+
+class _CustomPasswordFormFieldState extends State<CustomPasswordFormField> {
+  bool isHidden = true;
+
+  @override
+  Widget build(BuildContext context) {
+    String? finalHelperText;
+
+    if (widget.helperText != null && widget.counterText != null) {
+      finalHelperText = '\u200B' * 250 + widget.counterText!;
+    } else {
+      finalHelperText = widget.helperText;
+    }
+
+    return CustomTextFormField(
+      maxLength: widget.maxLength,
+      helperText: finalHelperText,
+      counterText: (widget.counterText != null && widget.helperText == null)
+          ? null
+          : widget.counterText,
+      validator: widget.validator,
+      obscureText: isHidden,
+      textEditingController: widget.textEditingController,
+      padding: widget.padding,
+      hintText: widget.hintText,
+      labelText: widget.labelText,
+      prefixIcon: widget.prefixIcon,
+      suffixIcon: InkWell(
+        borderRadius: BorderRadius.circular(23),
+        onTap: () {
+          setState(() {
+            isHidden = !isHidden;
+          });
+        },
+        child: Icon(
+          isHidden ? Icons.visibility_outlined : Icons.visibility_off_outlined,
+          size: 24.s,
+          color: AppColors.gray,
+        ),
+      ),
+    );
+  }
+}
---
+++ frontend/cut_map/lib/commom/widgets/custom_primary_buttom.dart
@@ -0,0 +1,57 @@
+import 'package:cut_map/commom/constants/app_colors.dart';
+import 'package:cut_map/commom/constants/app_fonts.dart';
+import 'package:cut_map/commom/extensions/sizes.dart';
+import 'package:flutter/material.dart';
+
+class CustomPrimaryButtom extends StatelessWidget {
+  final VoidCallback? onPressed;
+  final String text;
+  final Color? color;
+  final IconData? icon;
+
+  const CustomPrimaryButtom({
+    super.key,
+    this.onPressed,
+    required this.text,
+    this.color,
+    this.icon,
+  });
+
+  final BorderRadius _borderRadius = const BorderRadius.all(
+    Radius.circular(18),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Ink(
+      height: 60.h,
+      width: 382.w,
+      decoration: BoxDecoration(borderRadius: _borderRadius, color: color),
+      child: InkWell(
+        borderRadius: _borderRadius,
+        splashColor: Colors.white.withValues(alpha: 0.3),
+        highlightColor: Colors.blue.withValues(alpha: 0.5),
+        onTap: onPressed,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Text(text, style: AppFonts.bold16.apply(color: Colors.black)),
+            if (icon != null)
+              Positioned(
+                right: 8,
+                child: Container(
+                  width: 50.w,
+                  height: 50.h,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    shape: BoxShape.circle,
+                  ),
+                  child: Icon(icon, color: AppColors.gray, size: 30.s),
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
