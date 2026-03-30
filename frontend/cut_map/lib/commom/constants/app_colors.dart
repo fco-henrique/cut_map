70cecdbdff33a7d49cc36529fa68e54bd4d5884c
Henrique Nunes <henriquesilvanunes0015@gmail.com>
2026-03-30 12:02:53 -0300

    style(front): adicionado o arquivo com as cores da aplicação

---
+++ frontend/cut_map/lib/commom/constants/app_colors.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class AppColors {
+  AppColors._();
+
+  static const Color yellow = Color(0xFFF5C518);
+  static const Color darkGray = Color(0xFF2A2A2A);
+  static const Color gray = Color(0xFF888888);
+  static const Color background = Color(0xFF0D0D0D);
+}
