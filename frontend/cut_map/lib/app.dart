1be97e9e5e52e689c87637d91d77e6c5df369e0e
Henrique Nunes <henriquesilvanunes0015@gmail.com>
2026-03-30 22:02:17 -0300

    chore(front): adicionada a lib go_router para lidar com a navegação de telas

--- frontend/cut_map/lib/app.dart
+++ frontend/cut_map/lib/app.dart
@@ -1,9 +1,11 @@
 import 'package:cut_map/commom/extensions/sizes.dart';
 import 'package:cut_map/commom/routes/named_routes.dart';
 import 'package:cut_map/features/auth/screens/sign_up_screen.dart';
+import 'package:cut_map/features/auth/screens/verify_email_screen.dart';
 import 'package:cut_map/features/splash/splash_screen.dart';
 import 'package:cut_map/features/welcome/welcome_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -12,13 +14,31 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     Sizes.init(context);
 
-    return MaterialApp(
-      initialRoute: NamedRoutes.signUp,
-      routes: {
-        NamedRoutes.splash: (context) => SplashScreen(),
-        NamedRoutes.welcome: (context) => WelcomeScreen(),
-        NamedRoutes.signUp: (context) => SignUpScreen(),
-      },
+    return MaterialApp.router(
+      routerConfig: _router, // Aqui conectamos o GoRouter
+      debugShowCheckedModeBanner: false,
     );
   }
 }
+
+final _router = GoRouter(
+  initialLocation: NamedRoutes.signUp,
+  routes: [
+    GoRoute(
+      path: NamedRoutes.splash,
+      builder: (context, state) => const SplashScreen(),
+    ),
+    GoRoute(
+      path: NamedRoutes.welcome,
+      builder: (context, state) => const WelcomeScreen(),
+    ),
+    GoRoute(
+      path: NamedRoutes.signUp,
+      builder: (context, state) => const SignUpScreen(),
+    ),
+    GoRoute(
+      path: NamedRoutes.emailVerify,
+      builder: (context, state) => const VerifyEmailScreen(),
+    ),
+  ],
+);
