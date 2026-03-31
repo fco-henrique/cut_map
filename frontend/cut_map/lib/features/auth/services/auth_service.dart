0195d38080a4a7a2c6451dce661f08000f07308a
Henrique Nunes <henriquesilvanunes0015@gmail.com>
2026-03-31 14:23:19 -0300

    feat(front): implementando o authService com a função sign up

---
+++ frontend/cut_map/lib/features/auth/services/auth_service.dart
@@ -0,0 +1,58 @@
+import 'dart:developer';
+
+import 'package:cut_map/services/api_client.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+
+enum AuthStateType { uninitialized, authenticated, unauthenticated }
+
+class AuthState {
+  final AuthStateType type;
+  final String? role;
+
+  const AuthState({required this.type, this.role});
+}
+
+class AuthService with ChangeNotifier {
+  final ApiClient api;
+
+  AuthService({required this.api,});
+
+  Future<void> signUp({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    log("Os dados chegaram no service name: $name email: $email password: $password");
+    try {
+      final response = await api.dio.post(
+        '/user',
+        data: {
+          'name': name,
+          'email': email,
+          'password': password,
+        },
+      );
+
+      log("Usuário criado com sucesso: ${response.data}");
+    } on DioException catch (e) {
+      log("Erro do servidor: ${e.response?.data}");
+
+      String errorMessage = 'Ocorreu um erro de comunicação com o servidor.';
+
+      if (e.response?.data != null) {
+        var responseData = e.response!.data;
+
+        if (responseData is Map && responseData.containsKey('detail')) {
+          errorMessage = responseData['detail'];
+        } else if (responseData is String) {
+          errorMessage = responseData;
+        }
+      } else if (e.message != null) {
+        errorMessage = e.message!;
+      }
+
+      throw Exception(errorMessage);
+    }
+  }
+}
