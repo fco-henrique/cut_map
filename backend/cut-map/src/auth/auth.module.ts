371998e08b5fab15f32d1b3b249973fe7bb2fe68
Henrique Nunes <henriquesilvanunes0015@gmail.com>
2026-04-15 20:42:12 -0300

    config(back): configurando o jwt e as dependências do módulo de auth

--- backend/cut-map/src/auth/auth.module.ts
+++ backend/cut-map/src/auth/auth.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/user/entities/user.entity';
+import { RefreshToken } from './entities/refresh-token.entity';
+import { ConfigModule } from '@nestjs/config';
+import jwtConfig from './config/jwt.config';
+import { JwtModule } from '@nestjs/jwt';
+import { Argon2Service } from './hashing/argon2.service';
+import { HashingService } from './hashing/hashing.service';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([User, RefreshToken]),
+    ConfigModule.forFeature(jwtConfig),
+    JwtModule.registerAsync(jwtConfig.asProvider()),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    Argon2Service,
+    {
+      provide: HashingService,
+      useClass: Argon2Service,
+    },
+  ],
+  exports: [JwtModule, ConfigModule, HashingService],
 })
 export class AuthModule {}
