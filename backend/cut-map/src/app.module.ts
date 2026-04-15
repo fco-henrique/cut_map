83e8b77d3c9189403d3614ce49fe8a2422be1559
Henrique Nunes <henriquesilvanunes0015@gmail.com>
2026-04-15 19:53:06 -0300

    feat(back): subindo módulo de auth

--- backend/cut-map/src/app.module.ts
+++ backend/cut-map/src/app.module.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 import appConfig from './app.config';
 
 @Module({
@@ -26,6 +27,7 @@ import appConfig from './app.config';
       },
     }),
     UserModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
