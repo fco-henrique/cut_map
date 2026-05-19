bf98c2d260c6afda891335355107cc7beb2a6bfa
Henrique Nunes <henriquesilvanunes0015@gmail.com>
2026-05-19 20:23:58 -0300

    feat(back): adicionando decorator e guard para as Roles do sistema

---
+++ backend/cut-map/src/barbershop-member/decorators/roles.decorator.ts
@@ -0,0 +1,6 @@
+import { SetMetadata } from '@nestjs/common';
+import { BarbershopRole } from '../entities/barbershop-member.entity';
+
+export const ROLES_KEY = 'roles';
+export const Roles = (...roles: BarbershopRole[]) =>
+  SetMetadata(ROLES_KEY, roles);
---
+++ backend/cut-map/src/barbershop-member/guards/barbershop-roles.guard.ts
@@ -0,0 +1,60 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+} from '@nestjs/common';
+import { Request } from 'express';
+import { Reflector } from '@nestjs/core';
+import { ROLES_KEY } from '../decorators/roles.decorator';
+import { BarbershopRole } from '../entities/barbershop-member.entity';
+import { BarbershopMemberService } from '../barbershop-member.service';
+
+interface RequestWithUser extends Request {
+  user?: {
+    sub: string;
+  };
+}
+
+@Injectable()
+export class BarbershopRolesGuard implements CanActivate {
+  constructor(
+    private reflector: Reflector,
+    private membersService: BarbershopMemberService,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const requiredRoles = this.reflector.getAllAndOverride<BarbershopRole[]>(
+      ROLES_KEY,
+      [context.getHandler(), context.getClass()],
+    );
+
+    if (!requiredRoles) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest<RequestWithUser>();
+
+    const user = request.user;
+
+    const barbershopId = request.params.id as string;
+
+    if (!user || !barbershopId) {
+      throw new ForbiddenException('Acesso negado.');
+    }
+
+    const hasPermission = await this.membersService.checkIfUserHasRole(
+      barbershopId,
+      user.sub,
+      requiredRoles,
+    );
+
+    if (!hasPermission) {
+      throw new ForbiddenException(
+        'Você não tem permissão para realizar esta ação nesta barbearia.',
+      );
+    }
+
+    return true;
+  }
+}
