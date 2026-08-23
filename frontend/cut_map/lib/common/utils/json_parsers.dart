53a613271d413166c19d4309ecc74120cd5d2e15
Henrique Nunes <henriquesilvanunes0015@gmail.com>
2026-08-22 21:59:59 -0300

    chore(front): adicionando um util para a conversão de json

---
+++ frontend/cut_map/lib/common/utils/json_parsers.dart
@@ -0,0 +1,8 @@
+class JsonParsers {
+  static double? toDoubleOrNull(dynamic value) {
+    if (value == null) return null;
+    if (value is num) return value.toDouble();
+    if (value is String) return double.tryParse(value);
+    return null;
+  }
+}
